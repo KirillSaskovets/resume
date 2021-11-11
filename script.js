f584082174aafcab7fe1f20b16eeb7498b6edea1
KirillSaskovets <82257218+KirillSaskovets@users.noreply.github.com>
2021-11-11 04:21:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,140 @@
+var fl1=document.getElementById("intro");
+var fl2=document.getElementById("education");
+var fl3=document.getElementById("portfolio");
+var fl4=document.getElementById("work");
+var fl5=document.getElementById("skills");
+var fl6=document.getElementById("interest");
+var fl7=document.getElementById("contact");
+var fl8=document.getElementById("cookie2");
+
+function menu() {
+        var el=document.getElementById("menu");
+        if(el.style.display=="block"){
+        el.style.display="none";
+        } else {
+        el.style.display="block";
+        fl1.style.display="none";
+        fl2.style.display="none";
+        fl3.style.display="none";
+        fl4.style.display="none";
+        fl5.style.display="none";
+        fl6.style.display="none";
+        fl7.style.display="none";
+        fl8.style.display="none";
+}}
+
+function main() {
+        var el=document.getElementById("main");
+        var fl=document.getElementById("menu");
+        if(el.style.display=="block"){
+        el.style.display="none";
+        } else {
+        el.style.display="block";
+        fl.style.display="none";
+        fl1.style.display="none";
+        fl2.style.display="none";
+        fl3.style.display="none";
+        fl4.style.display="none";
+        fl5.style.display="none";
+        fl6.style.display="none";
+        fl7.style.display="none";
+        fl8.style.display="none";
+}
+}
+
+function intro() {
+        var el=document.getElementById("intro");
+        var fl=document.getElementById("menu");
+        if(el.style.display=="block"){
+        el.style.display="none";
+        } else {
+        el.style.display="block";
+        fl.style.display="none";
+}
+}
+
+function education() {
+        var el=document.getElementById("education");
+        var fl=document.getElementById("menu");
+        if(el.style.display=="block"){
+        el.style.display="none";
+        } else {
+        el.style.display="block";
+        fl.style.display="none";
+        
+}
+}
+
+function portfolio() {
+        var el=document.getElementById("portfolio");
+        var fl=document.getElementById("menu");
+        if(el.style.display=="block"){
+        el.style.display="none";
+        } else {
+        el.style.display="block";
+        fl.style.display="none";
+        
+}
+}
+
+function work() {
+        var el=document.getElementById("work");
+        var fl=document.getElementById("menu");
+        if(el.style.display=="block"){
+        el.style.display="none";
+        } else {
+        el.style.display="block";
+        fl.style.display="none";
+        
+}
+}
+
+function skills() {
+        var el=document.getElementById("skills");
+        var fl=document.getElementById("menu");
+        if(el.style.display=="block"){
+        el.style.display="none";
+        } else {
+        el.style.display="block";
+        fl.style.display="none";
+        
+}
+}
+
+function interest() {
+        var el=document.getElementById("interest");
+        var fl=document.getElementById("menu");
+        if(el.style.display=="block"){
+        el.style.display="none";
+        } else {
+        el.style.display="block";
+        fl.style.display="none";
+        
+}
+}
+
+function contact() {
+        var el=document.getElementById("contact");
+        var fl=document.getElementById("menu");
+        if(el.style.display=="block"){
+        el.style.display="none";
+        } else {
+        el.style.display="block";
+        fl.style.display="none";
+        
+}
+}
+
+
+function cookie2() {
+        var el=document.getElementById("cookie2");
+        var fl=document.getElementById("menu");
+        if(el.style.display=="block"){
+        el.style.display="none";
+        } else {
+        el.style.display="block";
+        fl.style.display="none";
+    
+}}
+
+
